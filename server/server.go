199cbeb4c5e76e23df57ef1c2bc8ed625585f707
Matthias <gh@maddosaurus.de>
2022-04-05 14:03:25 +0200

    Add rudimentary validation of entries and specify details in protobuf

--- server/server.go
+++ server/server.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"errors"
+	"fmt"
 	"log"
 	"net"
+	"strings"
 
 	pgsql "github.com/Maddosaurus/gotp/db"
 	cm "github.com/Maddosaurus/gotp/lib"
 	pb "github.com/Maddosaurus/gotp/proto/gotp"
+	"github.com/gofrs/uuid"
 	"google.golang.org/grpc"
 )
 
@@ -31,8 +34,10 @@ func (s *gOTPServer) ListEntries(uuid *pb.UUID, stream pb.GOTP_ListEntriesServer
 }
 
 func (s *gOTPServer) AddEntry(ctx context.Context, newEntry *pb.OTPEntry) (*pb.OTPEntry, error) {
-	// FIXME: Add verification of UUID / Secret
-	// FIXME: Salt & Hash Secret!
+	// FIXME: Encrypt Secret!
+	if err := verifyEntry(newEntry); err != nil {
+		return nil, fmt.Errorf("AddEntry: error verifying entry! %w", err)
+	}
 	if err := s.db.AddEntry(newEntry); err != nil {
 		return nil, err
 	}
@@ -40,10 +45,12 @@ func (s *gOTPServer) AddEntry(ctx context.Context, newEntry *pb.OTPEntry) (*pb.O
 }
 
 func (s *gOTPServer) UpdateEntry(ctx context.Context, candidate *pb.OTPEntry) (*pb.OTPEntry, error) {
-	// FIXME: Add verification of UUID / Secret
-	// FIXME: Salt & Hash Secret!
+	// FIXME: Encrypt Secret!
 	if tes, _ := s.db.GetEntry(&candidate.Uuid); tes == nil {
 		return nil, errors.New("Update candidate not found in DB!")
+	}
+	if err := verifyEntry(candidate); err != nil {
+		return nil, fmt.Errorf("AddEntry: error verifying entry! %w", err)
 	}
 	if err := s.db.UpdateEntry(candidate); err != nil {
 		return nil, err
@@ -63,6 +70,18 @@ func newServer() *gOTPServer {
 	s.db = &pgsql.PgSQL{}
 	s.db.InitDB()
 	return s
+}
+
+func verifyEntry(candiate *pb.OTPEntry) error {
+	if _, err := uuid.FromString(candiate.Uuid); err != nil {
+		return fmt.Errorf("verifyEntry: failed to verify UUID: %w", err)
+	}
+	if len(candiate.SecretToken) != 16 || strings.Compare(candiate.SecretToken, strings.ToUpper(candiate.SecretToken)) != 0 {
+		return errors.New("Error while verifying token! Ensure it is 16 chars of upper case ASCII!")
+	}
+	// FIXME: Add OTP verification, but no error handling :/
+	// https://github.com/xlzd/gotp/issues/18
+	return nil
 }
 
 func main() {

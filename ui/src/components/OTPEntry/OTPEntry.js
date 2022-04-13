74a2ba21aa1411804ea44c0f25c5b3dea012d5b4
Matthias <gh@maddosaurus.de>
2022-04-13 22:17:11 +0200

    UI progress: Introduce actual TOTP / HOTP functionality

--- ui/src/components/OTPEntry/OTPEntry.js
+++ ui/src/components/OTPEntry/OTPEntry.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 
+const jsotp = require('jsotp');
+
 
 export default function OTPEntry() {
   let params = useParams();
@@ -15,15 +17,37 @@ export default function OTPEntry() {
         .catch(console.log)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []) // This will lead to useEffect only being called once, vs an infinite loop
-    // https://stackoverflow.com/questions/53715465/can-i-set-state-inside-a-useeffect-hook
+           // https://stackoverflow.com/questions/53715465/can-i-set-state-inside-a-useeffect-hook
 
-    return (
-      <>
-        <h1>{entry.name}</h1>
-        <small>Updated At: {entry.updateTime}</small><br />
-        <small>Type: {entry.type}</small>
-        <h3>123456</h3>
-        <Link to={"/"}>Back to home page</Link>
-      </>
-    )
+
+    if (entry.type === "TOTP") {
+      const totp = jsotp.TOTP(entry.secretToken);
+      const n = totp.now();
+      return (
+        <>
+          <h1>{entry.name}</h1>
+          <small>Updated At: {entry.updateTime}</small><br />
+          <small>Type: {entry.type}</small>
+
+          <h3>{n}</h3>
+          <h2>FIXME: Implement dynamic update and countdown</h2>
+          <Link to={"/"}>Back to home page</Link>
+        </>
+      )
+    } else if (entry.type === "HOTP"){
+      const hotp = jsotp.HOTP(entry.secretToken);
+      const n = hotp.at(entry.counter);
+      return (
+        <>
+          <h1>{entry.name}</h1>
+          <small>Updated At: {entry.updateTime}</small><br />
+          <small>Type: {entry.type}</small>
+
+          <h3>{n}</h3>
+          <h2>FIXME: Implement actual counter update to server :3</h2>
+          <h2>Idea: Tie this (and text rendering) to a callback button function</h2>
+          <Link to={"/"}>Back to home page</Link>
+        </>
+      )
+    }
   }
